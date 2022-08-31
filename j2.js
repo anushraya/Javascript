25de43a261f930cbe2664ea4ce964ea0ca0f97dd
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-08-31 22:10:47 +0530

    Create j2.js

---
+++ j2.js
@@ -0,0 +1,17 @@
+// //declaration of variable
+// var firstname="anu";
+// //using variable
+// console.log(firstname);
+// //change value
+// firstname="mohit"
+// console.log(firstname)
+
+
+"use strict"      //we have to use keyword var if we use this statement use strict else using var keyword is optional.
+//declaration of variable
+var firstname="anu";
+//using variable
+console.log(firstname);
+//change value
+firstname="mohit"
+console.log(firstname)
