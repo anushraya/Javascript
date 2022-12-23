059112aeb77fbf758c38afcdeacbb0203d4abcf5
Anushraya Sharma <85569386+anushraya@users.noreply.github.com>
2022-12-23 23:16:53 +0530

    Create appendArray.js

---
+++ appendArray.js
@@ -0,0 +1,16 @@
+x=[1,2.2,"apple",null,undefined]
+x.push("kiwi")
+console.log(x)
+
+
+y=[1,2.2,"apple",null,undefined,"hii"]
+z=y.pop()
+console.log(y)
+console.log(z)
+
+x.unshift("hello")
+console.log(x)
+
+y=x.shift()
+console.log(x)
+console.log(y)
